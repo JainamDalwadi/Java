1ab635cf5c2a9f091a68f044b0b8d95f46b05e7a
Jainam Dalwadi <jainamdalwadi8@gmail.com>
2024-08-12 10:58:52 +0530

    adding parametarized constructor

--- constructor.java
+++ constructor.java
@@ -9,11 +9,21 @@ class sample {
         x = 4;
         y = 5;
     }
-}
 
+sample(int p, int q){
+x=p;
+y=q;
+}
+}
 class summation {
     public static void main(String args[]) {
+System.out.println("Calling default constructor");
+
         sample a = new sample();
         a.sum(); // Call the sum method to print the sum of x and y
+System.out.println("Calling perametarized constructor");
+
+     sample b = new sample(8,8);
+     b.sum();
     }
 }
